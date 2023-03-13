58ca3460ce0165712ae743567ed582f4fe3c9251
jinyong <zhaqh146@naver.com>
2023-03-13 17:49:12 +0900

    feat: styled-components 적용 및 이미지 배열 랜덤 정렬 완료

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,43 +1,59 @@
-import "../Game.css";
+import styled from "styled-components";
 import Card from "../components/Card";
 import image1 from "../images/image1.jpg";
 import image2 from "../images/image2.jpg";
 import image3 from "../images/image3.jpg";
+import image4 from "../images/image4.jpg";
+import image5 from "../images/image5.jpg";
+import image6 from "../images/image6.jpg";
+import image7 from "../images/image7.jpg";
+import image8 from "../images/image8.jpg";
+
+const Board = styled.div`
+  width: 500px;
+  height: 600px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-content: space-between;
+`;
 
 type BoardType = {
   id: number,
   image: string 
 }
 
+const initBoard: BoardType[] = [
+  {id: 0, image: image1},
+  {id: 0, image: image1},
+  {id: 1, image: image2},
+  {id: 1, image: image2},
+  {id: 2, image: image3},
+  {id: 2, image: image3},
+  {id: 3, image: image4},
+  {id: 3, image: image4},
+  {id: 4, image: image5},
+  {id: 4, image: image5},
+  {id: 5, image: image6},
+  {id: 5, image: image6},
+  {id: 6, image: image7},
+  {id: 6, image: image7},
+  {id: 7, image: image8},
+  {id: 7, image: image8},
+];
+
 function Game () {
-  const gameBoard: BoardType[] = [
-    {id: 0, image: image1},
-    {id: 0, image: image1},
-    {id: 1, image: image2},
-    {id: 1, image: image2},
-    {id: 2, image: image3},
-    {id: 2, image: image3},
-    {id: 3, image: image1},
-    {id: 3, image: image1},
-    {id: 4, image: image2},
-    {id: 4, image: image2},
-    {id: 5, image: image3},
-    {id: 5, image: image3},
-    {id: 6, image: image1},
-    {id: 6, image: image1},
-    {id: 7, image: image2},
-    {id: 7, image: image2},
-  ];
+  const gameBoard = initBoard.sort(() => Math.random() - 0.5);
 
   return (
-    <div className="GameBoard">
-      짝 맞추기 게임
-      <div className="CardArea">
+    <>
+      <div>짝 맞추기 게임</div>
+      <Board>
         {gameBoard.map((element, index) => (
           <Card key={index} id={element.id} image={element.image}/>
         ))}
-      </div>
-    </div>
+      </Board>
+    </>
   )
 };
 
